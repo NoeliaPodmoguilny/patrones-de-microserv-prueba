0fff7838f2dd927585e17be9ff220524e32592df
NoePodmoguilny <npodmoguilny@gmail.com>
2024-04-07 09:34:03 -0300

    cities and hotels app

--- eurekaserv/src/test/java/com/patronesMicroserv/loadBalancing/LoadBalancingApplicationTests.java
+++ eurekaserv/src/test/java/com/patronesMicroserv/loadBalancing/LoadBalancingApplicationTests.java
@@ -2,8 +2,12 @@ package com.patronesMicroserv.loadBalancing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
+@ContextConfiguration(classes = testConfig.class)
 class LoadBalancingApplicationTests {
 
 	@Test
---
+++ eurekaserv/src/test/java/com/patronesMicroserv/loadBalancing/testConfig.java
@@ -0,0 +1,9 @@
+
+package com.patronesMicroserv.loadBalancing;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class testConfig {
+    
+}
